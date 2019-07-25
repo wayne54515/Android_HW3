4183bfc5c22940189f30c585766119019ad86b74
wayne54515 <0920005wayne@gmail.com>
2019-07-25 23:57:45 +0800

    Many Update
    Toast實行
    ImageView顯示image
    依Array新增ListView item

--- app/src/main/java/c2/android_hw3/MainActivity.java
+++ app/src/main/java/c2/android_hw3/MainActivity.java
@@ -2,81 +2,100 @@ package c2.android_hw3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
-
+import android.widget.AdapterView;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends Activity {
 
     private ListView mListView;
+    private ArrayList<Map<String, Object>> arr_list = new ArrayList<Map<String, Object>>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mListView = (ListView) findViewById(R.id.list);
-        mListView.setAdapter(new ViewAdapter());
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectNetwork()
+                .penaltyLog()
+                .build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .penaltyLog()
+                .penaltyDeath()
+                .build());
+
+        try {
+            String result = db.executeQuery("SELECT * FROM user");
+
+            JSONArray jsonArray = new JSONArray(result);
+            for(int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonData = jsonArray.getJSONObject(i);
+                TableRow tr = new TableRow(MainActivity.this);
+//                tr.setLayoutParams(row_layout);
+//                tr.setGravity(Gravity.CENTER_HORIZONTAL);
+
+//                TextView user_acc = new TextView(MainActivity.this);
+//                user_acc.setText(jsonData.getString("acc"));
+//                user_acc.setLayoutParams(view_layout);
+
+//                TextView user_pwd = new TextView(MainActivity.this);
+//                user_pwd.setText(jsonData.getString("pwd"));
+//                user_pwd.setLayoutParams(view_layout);
+//
+//                tr.addView(user_acc);
+//                tr.addView(user_pwd);
+//                user_list.addView(tr);
+            }
+        } catch(Exception e) {
+            // Log.e("log_tag", e.toString());
+        }
+
+        final String[] json_list = {"u1", "u2", "u3", "u4", "u5"};
+        String[] json_list2 = {"a1", "a2", "a3", "a4", "a5"};
+
+        for(int i=0 ; i<json_list.length ; i++){
+            Map<String, Object> item = new HashMap<String, Object>();
+            item.put("object", json_list[i]);
+            item.put("image", json_list2[i]);
+
+            arr_list.add(item);
+        }
+
+        mListView.setAdapter(new ViewAdapter(MainActivity.this, arr_list));
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, json_list[position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    public class ViewAdapter extends BaseAdapter {
-
-        //取得數量
-        @Override
-        public int getCount() {
-            return 3;
-        }
-        //取得Item
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View v = convertView;
-            Holder holder;
-            if(v == null){
-                v = getLayoutInflater().inflate(R.layout.adapter, null);
-                holder = new Holder();
-                holder.image = (ImageView) v.findViewById(R.id.image);
-                holder.text = (TextView) v.findViewById(R.id.name);
-
-                v.setTag(holder);
-            }
-            else{
-                holder = (Holder) v.getTag();
-            }
-            switch(position) {
-                case 0:
-                    holder.image.setImageResource(R.drawable.a1);
-                    holder.text.setText("cat");
-                    break;
-                case 1:
-                    holder.image.setImageResource(R.drawable.a2);
-                    holder.text.setText("monkey");
-                    break;
-                case 2:
-                    holder.image.setImageResource(R.drawable.a3);
-                    holder.text.setText("panda");
-                    break;
-            }
-            return v;
-        }
-
-        class Holder{
-            ImageView image;
-            TextView text;
-        }
+    public void toast_user_name(View v){
+        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
     }
+
 }
--- app/src/main/java/c2/android_hw3/ViewAdapter.java
+++ app/src/main/java/c2/android_hw3/ViewAdapter.java
@@ -10,6 +10,66 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.Map;
+
+public class ViewAdapter extends BaseAdapter {
+
+    private LayoutInflater mLayInf;
+    List<Map<String, Object>> mItemList;
+    public ViewAdapter(Context context,  List<Map<String, Object>> itemList)
+    {
+        mLayInf = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mItemList = itemList;
+    }
+
+    @Override
+    public int getCount()
+    {
+        //取得 ListView 列表 Item 的數量
+        return mItemList.size();
+    }
+
+    @Override
+    public Object getItem(int position)
+    {
+        //取得 ListView 列表於 position 位置上的 Item
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position)
+    {
+        //取得 ListView 列表於 position 位置上的 Item 的 ID
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        //設定與回傳 convertView 作為顯示在這個 position 位置的 Item 的 View。
+        View v = mLayInf.inflate(R.layout.adapter, parent, false);
+
+        ImageView imgView = (ImageView) v.findViewById(R.id.image);
+        TextView txtView = (TextView) v.findViewById(R.id.name);
+
+        Context context = imgView.getContext();
+
+        int resId = context.getResources()
+                .getIdentifier(
+                        mItemList.get(position).get("image").toString()
+                        ,"drawable"
+                        ,context.getPackageName());
+
+        imgView.setImageResource(resId);
+        txtView.setText(mItemList.get(position).get("object").toString());
+
+        return v;
+    }
+
+}
 
 
 
